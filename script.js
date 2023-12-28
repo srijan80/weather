afc2e7857a48c3c5499e2d67bcdf9dfb54e4fcb4
shree <92163067+srijan80@users.noreply.github.com>
2023-12-28 21:49:43 +0545

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const weatherInfoDiv = document.querySelector("#weather-info");
 btn.addEventListener("click", function(){
     getWeather()
 })
-
 function getWeather() {
     const location = locationInput.value;
 
@@ -15,26 +14,20 @@ function getWeather() {
         alert("Please enter a location");
         return;
     }
-
     const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodeURIComponent(location)}`;
-
     fetch(apiUrl)
         .then(res => res.json())
         .then(data => {
-
             h1.innerHTML=`Weather in ${data.location.name}, ${data.location.country}`
             weatherInfoDiv.innerHTML =
             `<p> Temp: ${data.current.temp_c}°C</p>
             <p> localtime: ${data.location.localtime}</p>`
-             
-
+            
             //incase if you wanna checkout more detail
             console.log(data)           
 
         })
         .catch(error => {
-            
             h1.textContent = `You are a piece of shit, you don't even know a location.`
-            
         });
 }
